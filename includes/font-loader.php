759b82b57f72c7c0aa4fafbd7a4450d4754d0aa7
Tasnim Alam <tasnimalamcse@gmail.com>
2020-07-26 12:03:12 +0600

    Change font loader prefix EB => Social

--- includes/font-loader.php
+++ includes/font-loader.php
@@ -8,7 +8,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-class EB_Font_Loader
+class Social_Font_Loader
 {
 
     private static $instance;
@@ -91,4 +91,4 @@ class EB_Font_Loader
         }
     }
 }
-EB_Font_Loader::register();
+Social_Font_Loader::register();
